7742eabea56836fd2886f5bf4680f3f683d82bb9
Kalaidgi <kalaikannan690@gmail.com>
2026-01-31 22:06:52 +0530

    Use Railway MYSQL_URL for database connection

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,13 +1,12 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-
-const db = mysql.createConnection(process.env.DATABASE_URL);
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 db.connect((err) => {
   if (err) {
